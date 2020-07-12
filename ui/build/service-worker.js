300ef5618b892989d752181b224cef21842d0283
zmomen <zalmomen01@gmail.com>
2020-07-12 18:15:59 -0500

    add everything

--- ui/build/service-worker.js
+++ ui/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f94c3c583139934fdcbab431fab3d75b.js"
+  "/precache-manifest.0dcec653ed6ef34ce8bd341cdef7f010.js"
 );
 
 self.addEventListener('message', (event) => {
