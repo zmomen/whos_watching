5ac5788c5dc90e2c62c365fa0bb0175e8278030d
zmomen <zalmomen01@gmail.com>
2021-04-22 21:10:59 -0500

    added table sorting

--- ui/src/components/main/UserPreferences.js
+++ ui/src/components/main/UserPreferences.js
@@ -39,6 +39,70 @@ export const UserPreferences = ({ match }) => {
       .catch((err) => console.warn("error adding", err));
   };
 
+
+  function sortTable(n) {
+    var table,
+      rows,
+      switching,
+      i,
+      x,
+      y,
+      shouldSwitch,
+      dir,
+      switchcount = 0;
+    table = document.getElementById("myTable");
+    switching = true;
+    //Set the sorting direction to ascending:
+    dir = "asc";
+    /*Make a loop that will continue until
+  no switching has been done:*/
+    while (switching) {
+      //start by saying: no switching is done:
+      switching = false;
+      rows = table.rows;
+      /*Loop through all table rows (except the
+    first, which contains table headers):*/
+      for (i = 1; i < rows.length - 1; i++) {
+        //start by saying there should be no switching:
+        shouldSwitch = false;
+        /*Get the two elements you want to compare,
+      one from current row and one from the next:*/
+        x = rows[i].getElementsByTagName("TD")[n];
+        y = rows[i + 1].getElementsByTagName("TD")[n];
+        /*check if the two rows should switch place,
+      based on the direction, asc or desc:*/
+        if (dir === "asc") {
+          if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+            //if so, mark as a switch and break the loop:
+            shouldSwitch = true;
+            break;
+          }
+        } else if (dir === "desc") {
+          if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+            //if so, mark as a switch and break the loop:
+            shouldSwitch = true;
+            break;
+          }
+        }
+      }
+      if (shouldSwitch) {
+        /*If a switch has been marked, make the switch
+      and mark that a switch has been done:*/
+        rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+        switching = true;
+        //Each time a switch is done, increase this count by 1:
+        switchcount++;
+      } else {
+        /*If no switching has been done AND the direction is "asc",
+      set the direction to "desc" and run the while loop again.*/
+        if (switchcount === 0 && dir === "asc") {
+          dir = "desc";
+          switching = true;
+        }
+      }
+    }
+  }
+
   return (
     <div className="main-body">
       <ul className={"menu"}>
@@ -74,13 +138,13 @@ export const UserPreferences = ({ match }) => {
         <li className={"divider"}></li>
         <li>
           {isAdding && <AddRow handleAdd={handleAdd} />}
-          <table className="table table-hover">
+          <table id="myTable" className="table table-hover">
             <thead>
               <tr>
-                <th>Title</th>
-                <th>Type</th>
-                <th>Genre</th>
-                <th>Notes</th>
+                <th onClick={() => sortTable(0)}>Title</th>
+                <th onClick={() => sortTable(1)}>Type</th>
+                <th onClick={() => sortTable(2)}>Genre</th>
+                <th onClick={() => sortTable(3)}>Notes</th>
                 <th></th>
               </tr>
             </thead>
